1e37fe306fe272c2288276ee8e58972b69cfb72a
abdulhammedabdulazeez <a.abdulazee@alustudent.com>
2023-05-19 04:52:51 -0700

    New commit

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\+?(\d+|[A-Z]\w+),\+?(\d+),(-1:0:-1:?0?:-1(:-1)?)/).join
+puts ARGV[0].scan(/[Sender:\+?(\d+|[A-Z]\w+)],[Receiver:\+?(\d+)],[(-1:0:-1:?0?:-1(:-1)?)/]).join
